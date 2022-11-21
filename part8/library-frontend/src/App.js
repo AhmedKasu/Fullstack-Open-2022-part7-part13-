24d8afabc086b4e8e29fc6ddc233d1c4c5a3f8e1
Ahmed Kasu <ankassu@gmail.com>
2022-11-21 15:01:47 +0200

    Exercise 8.8: Authors view

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import { useState } from 'react';
+import { useQuery } from '@apollo/client';
+
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+
+import { ALL_AUTHORS } from './queries';
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState('authors');
+  const result = useQuery(ALL_AUTHORS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div>
@@ -14,13 +23,13 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
 
       <Books show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
