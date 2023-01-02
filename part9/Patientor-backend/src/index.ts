4998abe03268b1df8da53da716f5075dfbccd9cc
Ahmed Kasu <ankassu@gmail.com>
2023-01-02 13:55:05 +0200

    Exercise 9.10: Patientor backend, step3

---
+++ part9/Patientor-backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import { Request } from 'express';
+
+import diagnosesRouter from './routes/diagnoses';
+
+const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors<Request>());
+app.use(express.json());
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diagnoses', diagnosesRouter);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ part9/Patientor-backend/src/services/diagnoses.ts
@@ -0,0 +1,8 @@
+import { Diagnose } from '../types';
+import diagnosesData from '../../data/diagnoses.json';
+
+const getDiagnoses = (): Diagnose[] => {
+  return diagnosesData;
+};
+
+export default { getDiagnoses };
